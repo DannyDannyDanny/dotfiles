25510d88baf4b14576518187ca823a3f04634ae2
DannyDannyDanny <dnth@lundbeck.com>
2023-06-08 09:02:09 +0200

    vimwiki: dth @ DK023900WSL

--- install.sh
+++ install.sh
@@ -30,26 +30,27 @@ rm .zshrc
 # $HOME/.dotfiles.base/bin/dotfiles.symlink install
 
 echo >&2 "====================================================================="
-echo >&2 " installing tmux"
+echo >&2 " >> installing tmux"
 sudo apt-get install -y tmux=3.0a-2ubuntu0.4
 
 # I'd like to use fish, please
 echo >&2 "====================================================================="
-echo >&2 " installing fish"
+echo >&2 " >> installing fish"
 sudo apt-get install -y fish
 sudo chsh -s /usr/bin/fish $USER
 
 # Install fzf
 echo >&2 "====================================================================="
-echo >&2 " installing fzf"
+echo >&2 " >> installing fzf"
 FZF_VERSION=0.40.0
 curl -L https://github.com/junegunn/fzf/releases/download/${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz | tar xzC /bin
 
 # Install neovim
 echo >&2 "====================================================================="
-echo >&2 " installing nvim"
-NVIM_VERSION=0.7.2
-sudo apt-get install -y libfuse2
-mkdir /bin/nvim
-curl -L -o /bin/nvim https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim.appimage
-chmod a+x /bin/nvim
+echo >&2 " >> installing nvim"
+# NVIM_VERSION=0.7.2
+sudo apt-get install -y neovim=0.7.2-3~bpo20.04.1~ppa1
+# sudo apt-get install -y libfuse2
+# mkdir /bin/nvim
+# curl -L -o /bin/nvim https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim.appimage
+# chmod a+x /bin/nvim
