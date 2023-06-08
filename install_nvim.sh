6fdb57ab636cece91387d60e0161413582cc231e
DannyDannyDanny <dnth@lundbeck.com>
2023-06-08 14:23:28 +0200

    :bug: fix nvim dl url

--- install_nvim.sh
+++ install_nvim.sh
@@ -4,11 +4,11 @@
 echo >&2 "====================================================================="
 echo >&2 " >> installing nvim"
 # curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-NVIM_VERSION=0.9.0
+# NVIM_VERSION=0.9.0
 echo >&2 " >>> downloading nvim"
 # curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-curl -LO https://github.com/neovim/neovim/releases/tag/v${NVIM_VERSION}/download/nvim.appimage
+curl -LO https://github.com/neovim/neovim/releases/download/v0.9.0/nvim.appimage
 echo >&2 " >>> examine"
-cat nvim.appimage
+# cat nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage
