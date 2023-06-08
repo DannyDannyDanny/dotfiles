a94cfa4c3c9dcb250aec37f06f8e3cc71298da19
DannyDannyDanny <dnth@lundbeck.com>
2023-06-08 09:16:14 +0200

    vimwiki: dth @ DK023900WSL

--- install.sh
+++ install.sh
@@ -50,6 +50,6 @@ echo >&2 "====================================================================="
 echo >&2 " >> installing nvim"
 # curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 NVIM_VERSION=0.9.0
-curl -LO https://github.com/neovim/neovim/releases/tag/v{NVIM_VERSION}/download/nvim.appimage
+curl -LO https://github.com/neovim/neovim/releases/tag/v${NVIM_VERSION}/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage
