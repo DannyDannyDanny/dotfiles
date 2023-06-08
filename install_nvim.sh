fbcf94aa2279db3e8c9a8b477162d69d0d80f5e1
DannyDannyDanny <dnth@lundbeck.com>
2023-06-08 14:15:31 +0200

    :boom: work on nvim install script

--- install_nvim.sh
+++ install_nvim.sh
@@ -5,6 +5,10 @@ echo >&2 "====================================================================="
 echo >&2 " >> installing nvim"
 # curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 NVIM_VERSION=0.9.0
+echo >&2 " >>> downloading nvim"
+# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 curl -LO https://github.com/neovim/neovim/releases/tag/v${NVIM_VERSION}/download/nvim.appimage
+echo >&2 " >>> examine"
+cat nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage
