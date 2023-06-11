00d0b76d00b3032281ce9824f68d41552c19bb23
dth@taiga.ai <DannyDannyDanny>
2023-06-11 20:54:19 +0200

    :memo: add end-log to fish install

--- install_fish.sh
+++ install_fish.sh
@@ -4,5 +4,8 @@
 echo >&2 "====================================================================="
 echo >&2 " >> installing fish"
 sudo apt-get install -y fish
+
 echo >&2 " >>> changing shell to fish"
 sudo chsh -s /usr/bin/fish $USER
+
+echo >&2 " >>> echo $0"
