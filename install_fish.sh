3a53578116f4f65af819d2a9350136369f3aa5ee
dth@taiga.ai <DannyDannyDanny>
2023-06-11 21:40:28 +0200

    :bug: fix bash var concat syntax

--- install_fish.sh
+++ install_fish.sh
@@ -4,7 +4,7 @@ FISH_VERSION=3.3.1+ds-3
 # I'd like to use fish, please
 echo >&2 "====================================================================="
 echo >&2 " >> installing fish"
-sudo apt-get install -y fish=$(FISH_VERSION)
+sudo apt-get install -y fish=${FISH_VERSION}
 
 echo >&2 " >>> changing shell to fish"
 sudo chsh -s /usr/bin/fish $USER
