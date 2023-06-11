1e6903d2aea2fba70a63df86aad9f0637dfeb933
dth@taiga.ai <DannyDannyDanny>
2023-06-11 20:35:50 +0200

    :boom: work on install fzf script

--- install_fzf.sh
+++ install_fzf.sh
@@ -4,4 +4,13 @@
 echo >&2 "====================================================================="
 echo >&2 " >> installing fzf"
 FZF_VERSION=0.40.0
-curl -L https://github.com/junegunn/fzf/releases/download/${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz | tar xzC /bin
+# curl -L https://github.com/junegunn/fzf/releases/download/${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz | tar xzC /bin
+
+echo >&2 " >>> downloading"
+curl -LO https://github.com/junegunn/fzf/releases/download/${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz
+
+echo >&2 " >>> extracting"
+tar xfv fzf-${FZF_VERSION}-linux_amd64.tar.gz
+
+echo >&2 " >>> $(ls -la)"
+# tar xzC /bin
