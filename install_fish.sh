580c7a948de61be4675780a517284d7744f94c38
dth@taiga.ai <DannyDannyDanny>
2023-06-12 22:18:19 +0200

    :pushpin: change fish version to match ubuntu20.04

--- install_fish.sh
+++ install_fish.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-FISH_VERSION=3.3.1+ds-3
+
+# fish version on ubuntu 22.04
+# FISH_VERSION=3.3.1+ds-3
+# fish version on ubuntu 20.04 (codespaces ubuntu version)
+FISH_VERSION=3.1.0-1.2
 
 # I'd like to use fish, please
 echo >&2 "====================================================================="
