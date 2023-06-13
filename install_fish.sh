1acf876793d87ba3c6ab8b4917cf14f1ae513fd6
Daniel Thoren <dth@taiga.ai>
2023-06-13 15:00:33 +0200

    :pushpin: :fish: pin fish dependency

--- install_fish.sh
+++ install_fish.sh
@@ -2,8 +2,10 @@
 
 # fish version on ubuntu 22.04
 # FISH_VERSION=3.3.1+ds-3
+# probably works on ubuntu 20.04 but couldn't be found in codespaces apt
+# FISH_VERSION=3.1.0-1.2
 # fish version on ubuntu 20.04 (codespaces ubuntu version)
-FISH_VERSION=3.1.0-1.2
+FISH_VERSION=3.1.2-3+deb11u1
 
 # I'd like to use fish, please
 echo >&2 "====================================================================="
