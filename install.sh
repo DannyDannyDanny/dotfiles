161fca2c466e99bc2977c9207a793f5018d45d2d
Daniel Thoren <dth@taiga.ai>
2023-07-03 13:13:44 +0200

    :sparkles: clone methdology repo into codespaces

--- install.sh
+++ install.sh
@@ -27,6 +27,7 @@ rm -rf .oh-my-zsh
 # mv .gitconfig .gitconfig.private
 
 git clone https://github.com/dannydannydanny/dotfiles ~/dotfiles
+git clone https://github.com/dannydannydanny/methodology ~/methodology
 # git clone https://github.com/dcreager/dotfiles-base .dotfiles.base
 # git clone https://github.com/dcreager/dotfiles-public -b codespaces --recurse-submodules .dotfiles.public
 # $HOME/.dotfiles.base/bin/dotfiles.symlink install
