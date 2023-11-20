10e25ac9c73206816db08aa010a0c507a3271fea
DannyDannyDanny <dth@taiga.ai>
2023-11-20 11:05:32 +0100

    :boom: auto-add github key to ssh-agent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,7 @@ if not pgrep --full ssh-agent | string collect > /dev/null
 	eval (ssh-agent -c)
 	set -Ux SSH_AGENT_PID $SSH_AGENT_PID
 	set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  # ssh-add ~/.ssh/id_*_github
 end
+
+set -gx EDITOR nvim
