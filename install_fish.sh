3d0ea130fe4632e49b9178b12ff78efb22e6c08f
DannyDannyDanny <dth@taiga.ai>
2024-02-03 19:51:07 +0100

    :boom: :pushpin: temporary pin fish version debian11

--- install_fish.sh
+++ install_fish.sh
@@ -4,10 +4,14 @@ set -e
 echo >&2 "====================================================================="
 echo >&2 " >> installing fish"
 
-echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_12/ /' | \
+# TODO: autodetect debian11/12
+# echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_12/ /' | \
+echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_11/ /' | \
   sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
 
-curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_12/Release.key | \
+# TODO: autodetect debian11/12
+# curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_12/Release.key | \
+curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | \
   gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
 
 sudo apt update
