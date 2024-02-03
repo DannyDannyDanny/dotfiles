b483ad4a6e551ac74b512beaf7beae5c09def31e
DannyDannyDanny <dth@taiga.ai>
2024-02-03 21:24:18 +0100

    :boom: draft python install fish script

---
+++ install_python.fish
@@ -0,0 +1,16 @@
+# TODO: turn this into an actual script
+# TODO: rename to install_python.fish
+
+# curl https://pyenv.run | bash
+# set -Ux PYENV_ROOT $HOME/.pyenv
+# fish_add_path $PYENV_ROOT/bin
+
+# now add:
+# pyenv init - | source
+# to:
+# ~/.config/fish/config.fish
+
+# install suggested build environment:
+# sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
+# libbz2-dev libreadline-dev libsqlite3-dev curl \
+# libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
