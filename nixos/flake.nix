d12b93664d5d8eb71763ae9bf35ab7f1423d8722
DannyDannyDanny <dth@taiga.ai>
2025-02-07 19:06:37 +0100

    :art: use flakes in dotfiles

---
+++ nixos/flake.nix
@@ -0,0 +1,15 @@
+
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        ./hardware-configuration.nix
+      ];
+    };
+  };
+}
+
