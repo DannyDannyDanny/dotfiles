ce6249997e8fab981fe31b6d3d0e837b1845a4d3
DannyDannyDanny <dth@taiga.ai>
2025-02-07 19:24:07 +0100

    :sparkles: add fish config from wsl branch

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,6 +10,7 @@
       ./hardware-configuration.nix
       ./tmux.nix
       # ./uxplay.nix
+      ./fish.nix
     ];
 
   # Bootloader.
---
+++ nixos/fish.nix
@@ -0,0 +1,71 @@
+{ config, pkgs, ... }:
+
+{
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      fish_vi_key_bindings
+      set fish_greeting 🐟: (set_color yellow; date +%T; set_color green; date --iso-8601; set_color normal)
+
+
+      # name: Default
+      # author: Lily Ballard
+      # edits: DannyDannyDanny
+      # ref: stackoverflow.com/a/61262358/5684214
+      
+      function fish_prompt --description 'Write out the prompt'
+        set -l last_pipestatus $pipestatus
+        set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+        set -l normal (set_color normal)
+        set -q fish_color_status
+        or set -g fish_color_status red
+      
+        # Color the prompt differently when we're root
+        set -l color_cwd $fish_color_cwd
+        set -l suffix '>'
+        if functions -q fish_is_root_user; and fish_is_root_user
+            if set -q fish_color_cwd_root
+                set color_cwd $fish_color_cwd_root
+            end
+            set suffix '#'
+        end
+    
+        # Write pipestatus
+        # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
+        set -l bold_flag --bold
+        set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+        if test $__fish_prompt_status_generation = $status_generation
+            set bold_flag
+        end
+        set __fish_prompt_status_generation $status_generation
+        set -l status_color (set_color $fish_color_status)
+        set -l statusb_color (set_color $bold_flag $fish_color_status)
+        set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+        set -l nix_shell_info (
+          if test -n "$IN_NIX_SHELL"
+            echo -n "🐚 "
+          end
+        )
+      
+        echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $nix_shell_info $suffix " "
+      end
+   
+    '';
+  };
+
+  programs.bash = {
+    interactiveShellInit = ''
+      # the first arguement in the if statement check that the parent process is not a fish shell.
+      # this allows spawning a bash shell inside a fish shell without the inner-most bash shell launching a fish shell
+      # however this also means that nix-shell starts a bash shell unless you use `nix-shell [args] --run fish`
+      # or run `fish` as the first command when entering nix-shell
+
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
+}
+
