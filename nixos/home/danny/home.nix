7a34ccfc7f060c3b5f3bd9e25340982a4845d043
DannyDannyDanny <dth@taiga.ai>
2026-01-11 15:04:22 +0100

    refactor: neovim config out of home-manager :art:

--- nixos/home/danny/home.nix
+++ nixos/home/danny/home.nix
@@ -6,75 +6,8 @@
 
   programs.home-manager.enable = true;
 
-  # Neovim (user-level, works great on macOS)
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    # extraLuaConfig is also available; we’ll keep your Vimscript as-is:
-    extraConfig = ''
-      set title
-      set mouse=a
-      set nohlsearch
-      set number
-      let mapleader=","
-
-      lua << EOF
-        local config_file = os.getenv("HOME")..'/.local/share/nvim_color_scheme'
-        local f = io.open(config_file, "r")
-        if f ~= nil then
-            local system_theme = f:read()
-            io.close(f)
-            if system_theme == 'dark' then
-                vim.cmd("set bg=dark")
-            elseif system_theme == 'light' then
-                vim.cmd("set bg=light")
-            else
-                print('warning: expected value "light" or "dark"')
-                print('  got:', system_theme)
-                print('  expected path:', config_file)
-            end
-        else
-            print('warning: nvim color scheme not found')
-            print('  expected path:', config_file)
-        end
-      EOF
-
-      colorscheme catppuccin " catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
-
-      " netrw (dir listing) settings
-      let g:netrw_liststyle = 3
-      let g:netrw_banner = 0
-      let g:netrw_browse_split = 3
-      let g:netrw_winsize = 25  " % of page
-
-      set listchars=tab:→\ ,space:·,nbsp:␣,trail:•,eol:¶,precedes:«,extends:»
-      set clipboard+=unnamedplus
-
-      " Replace-all is aliased to S.
-      nnoremap S :%s//g<Left><Left>
-
-      " save file with ,w
-      map <leader>w :w<cr><Space>
-
-      " spellcheck
-      set spell spelllang=en_us
-      setlocal spell! spelllang=en_us
-    '';
-
-    plugins = with pkgs.vimPlugins; [
-      vim-surround
-      vim-gitgutter
-      vim-nix
-      # vimtex - disabled due to build check issue with fzf-lua dependency
-      # vimtex-fixed
-      fzf-lua
-      nerdtree
-      rainbow
-      catppuccin-nvim
-      goyo-vim
-      limelight-vim
-    ];
-  };
+  # Import neovim configuration
+  imports = [ ../../neovim.nix ];
 
   # tmux (user-level; same config on macOS and NixOS if you reuse this file)
   programs.tmux = {
