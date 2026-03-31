9f73571f55a2a6466a67b449d364333da822a33a
DannyDannyDanny <dth@taiga.ai>
2026-03-31 15:33:23 +0200

    fix: restore bootloader config in phantom-ship hardware nix 🥾
    Accidentally stripped systemd-boot config when cleaning up duplicate
    fileSystems entries.

--- nixos/hosts/phantom-ship-hardware.nix
+++ nixos/hosts/phantom-ship-hardware.nix
@@ -3,6 +3,9 @@
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
